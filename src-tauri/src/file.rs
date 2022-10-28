d8e4c2b794efa192261d7cec196ec6e47c72ac37
kendyyin <xruistudy@gmail.com>
2022-10-28 19:32:01 +0800

    feat: 更新配置

--- src-tauri/src/file.rs
+++ src-tauri/src/file.rs
@@ -1,4 +1,10 @@
 use std::path::Path;
 
-// pub  file {
-// }
+pub fn get_file_list(_window: tauri::Window, path: &str) {}
+
+#[tauri::command]
+async fn get_folder_images<R: Runtime>(
+    app: tauri::AppHandle<R>,
+    window: tauri::Window<R>,
+) -> Result<(), String> {
+}
